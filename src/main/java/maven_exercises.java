88c03626f1def5c7226b2257b935aed5e03ef5ba
Michael Dickens <dickens.michael64@gmail.com>
2022-07-03 21:44:43 -0500

    trying to push up to github

---
+++ src/main/java/maven_exercises.java
@@ -0,0 +1,15 @@
+import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
+
+public class maven_exercises {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please type something: ");
+        String userInput = input.next();
+
+
+
+    }
+}
